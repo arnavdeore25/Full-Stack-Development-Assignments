6b0d38931c884406f4f15d2d9786e390984f5df5
Arnav Deore <156898120+arnavdeore25@users.noreply.github.com>
2026-08-17 17:30:28 +0530

    Modify Navbar to include logout functionality
    Updated Navbar component to accept onLogout prop and added logout button.

--- Lab 05 & 06/src/components/Navbar.jsx
+++ Lab 05 & 06/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 
 
-function Navbar({ darkMode, setDarkMode }) {
+function Navbar({onLogout }) {
   return (
     <nav className="bg-blue-600 text-white shadow-md">
       <div className="max-w-6xl mx-auto flex justify-between items-center px-5 py-4">
@@ -24,6 +24,10 @@ function Navbar({ darkMode, setDarkMode }) {
           <a href="#contact" className="hover:text-yellow-300">
             Contact
           </a>
+           <button
+  onClick={onLogout}
+  className="bg-red-500 px-4 py-2 rounded hover:bg-red-600"
+>
 
          
         </div>
