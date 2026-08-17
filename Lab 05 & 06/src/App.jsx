5ed2bd6aeef32e8d50a1a0b95afe7054f6eb70e0
Arnav Deore <156898120+arnavdeore25@users.noreply.github.com>
2026-08-17 17:30:49 +0530

    Implement login functionality in App component
    Added login functionality to the App component, replacing the study plans state with a login state. The app now conditionally renders the Login component based on the user's login status.

--- Lab 05 & 06/src/App.jsx
+++ Lab 05 & 06/src/App.jsx
@@ -2,35 +2,37 @@ import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Quote from "./components/Quote";
-import StudyList from "./components/StudyList";
 import Todo from "./components/Todo";
 import ContactForm from "./components/ContactForm";
 import Footer from "./components/Footer";
+import Login from "./components/Login";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
-  const [studyPlans, setStudyPlans] = useState([]);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const addPlan = (plan) => {
-    setStudyPlans([...studyPlans, plan]);
-  };
+
+  if (!isLoggedIn) {
+    return (
+      <Login onLogin={() => setIsLoggedIn(true)} />
+    );
+  }
+
 
   return (
     <div
-      className={`min-h-screen duration-300 ${
+      className={`min-h-screen ${
         darkMode
           ? "bg-gray-900 text-white"
           : "bg-gray-100 text-gray-900"
       }`}
     >
-      <Navbar />
+      <Navbar onLogout={() => setIsLoggedIn(false)} />
 
       <Hero />
 
       <div className="max-w-6xl mx-auto px-5 py-10 space-y-10">
         <Quote />
-
-
 
         <div id="todo">
           <Todo />
